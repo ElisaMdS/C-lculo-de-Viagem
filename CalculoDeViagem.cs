27e950faf6bcf3007cbf7d89db4a7861811af721
ElisaMdS <50852838+ElisaMdS@users.noreply.github.com>
2021-02-06 02:47:29 -0300

    Add files via upload

---
+++ CalculoDeViagem.cs
@@ -0,0 +1,27 @@
+/*using System;
+
+public class CalculodeViagem {
+
+    double litros = 0;
+
+    double Calculo(int horas, int velocidadeMedia) {
+
+        double kilometrosPorLitro = 12;
+
+        double distancia = velocidadeMedia * horas;
+        litros = distancia/kilometrosPorLitro;
+
+        return litros;
+
+    }
+
+    static void main (string [] args) {
+        
+        int horas = Console.ReadLine();
+        int velocidadeMedia = Console.ReadLine();
+        
+        Calculo(horas, velocidadeMedia);
+        Console.WriteLine(litros);
+    }
+
+}*/
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class CalculodeViagem {
+
+    public static double litros = 0;
+
+    public static double Calculo(double horas, double velocidadeMedia) {
+
+        double kilometrosPorLitro = 12;
+
+        double distancia = velocidadeMedia * horas;
+        litros = distancia/kilometrosPorLitro;
+
+        return litros;
+
+    }
+
+    static void Main (string[] args) {
+        var horaVelocidade = Console.ReadLine().Split();
+        double horas = Double.Parse(horaVelocidade[0]);
+        double velocidadeMedia = Double.Parse(horaVelocidade[1]);
+        
+        Calculo(horas, velocidadeMedia);
+        Console.WriteLine(string.Format("{0:0.000}", litros));
+    }
+
+}
